44ca145cc718aa9bcf3ae1965c3e53983c4d4fe2
Matixdg <mateusz.n1@onet.pl>
2024-09-12 20:10:04 +0200

    Added support for curse icon without MinimapAPI

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 local mod = RegisterMod("Curse of Champion Belt", 1)
 local game = Game()
---local curse_active=false
+local custom_curse_sprite = Sprite()
+custom_curse_sprite:Load("gfx/ui/curse_champion_belt.anm2", true)
+
 
 
 local MinimapAPI
@@ -15,24 +17,6 @@ if not status then
     MinimapAPI = nil
 end
 
-local custom_curse_sprite = Sprite()
-custom_curse_sprite:Load("gfx/ui/curse_champion_belt.anm2", true)
-
-
-
-function mod:is_curse_active()
-local level = game:GetLevel()
---print("dziala")
-if level:GetCurses()==8192 then 
-return true
-else
-return false
-
-end
-
-end
-
-
 
 
 
@@ -45,14 +29,22 @@ local Challenges ={
    CHALLENGE_CHAMPION = Isaac.GetChallengeIdByName("Championssssss...")
 }
 
+function mod:is_curse_active()
+	local level = game:GetLevel()
+	if level:GetCurses()== Curses.CURSE_CHAMPION  then 
+	return true
+	else
+	return false	
+	end
+	
+end
+	
 
 
 
 function mod:map_flag_icon(curse_id)
 
-if curse_id ~= 8192 then return end;
-
-
+if curse_id ~= Curses.CURSE_CHAMPION then return end;
 
 MinimapAPI:AddMapFlag(
 curse_id,
@@ -63,25 +55,27 @@ custom_curse_sprite,
 
 )
 
-
-
-
-
-
-
 end
+
+
+
+
+
+
+
+
 
 function mod:Curse(CurseFlags)
 
 
 if game.Challenge == Challenges.CHALLENGE_CHAMPION then 
-return 8192
+return Curses.CURSE_CHAMPION 
 end
 
 
 local level = game:GetLevel()
 local player = Isaac.GetPlayer(0)
-local base_chance = 90;
+local base_chance = 99;
 
 if player:HasCollectible(CollectibleType.COLLECTIBLE_CHAMPION_BELT) then 
 base_chance = base_chance + 7
@@ -91,31 +85,20 @@ if player:HasTrinket(TrinketType.TRINKET_PURPLE_HEART) then
 base_chance = base_chance + 7
 end 
 
-print(base_chance)
---print(MinimapAPI)
-
 local rand = math.random(0,99)
---print(rand)
+
 if rand >= 0 and rand <= base_chance and level:GetCurses() ==0 then
 	print(Curses.CURSE_CHAMPION)
 	print(level:GetCurseName())
 	if status then
 	mod:map_flag_icon(Curses.CURSE_CHAMPION)
+	
 	end
 	return Curses.CURSE_CHAMPION
-	--curse_active=true
+
 end
-
---curse_active=false
-
-print(level:GetCurses())
-
---[[
-	if level:GetCurses()==0 then 
-	return CURSE_CHAMPION
-	end
-]]--
-
+--print(rand)
+--print(level:GetCurses())
 
 
 end
@@ -124,7 +107,6 @@ function mod:Test()
 	local level = game:GetLevel()
 	if level:GetCurses() == Curses.CURSE_CHAMPION then 
 	local room = Game():GetRoom()
-    -- Iterate over all entities in the room
     local entities = Isaac.GetRoomEntities()
     for i = 1, #entities do
         local entity = entities[i]
@@ -152,9 +134,8 @@ function mod:Test()
 	
 	
         
-        -- Check if the entity is an enemy
         if entity:IsActiveEnemy() then
-            -- Make the entity a champion
+     
             entity:ToNPC():MakeChampion(1, rand, true)
 			entity:ToNPC():AddHealth(999)
         end
@@ -178,18 +159,18 @@ function mod:Gowno()
 	
 	if not MinimapAPI then
 
-	--print(Curses.CURSE_CHAMPION)
 
+	
             if mod:is_curse_active() and not Input.IsActionPressed(ButtonAction.ACTION_MAP, 0) then
                 local corner = Vector(Isaac.GetScreenWidth(), 0)
-                local HUDOffset = Options.HUDOffset * Vector(-20, 12)
+                local HUDOffset = Options.HUDOffset * Vector(-50, 12)
                 local curOffset = 0
                 -- Isaac.GetScreenHeight()
                 -- Isaac.GetScreenWidth()
 
         
                         custom_curse_sprite:SetFrame('Curse', 0)
-                        custom_curse_sprite:Render(corner + HUDOffset + Vector(-66, 10+(16*curOffset)), Vector.Zero, Vector.Zero)
+                        custom_curse_sprite:Render(corner + HUDOffset + Vector(-15, 60+(16*curOffset)), Vector.Zero, Vector.Zero)
                         curOffset = curOffset + 1
      
             end
@@ -201,7 +182,5 @@ end
 
 
 mod:AddCallback(ModCallbacks.MC_POST_CURSE_EVAL, mod.Curse)
---mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, Curses.OnUpdate)
 mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.Test)
---mod:AddCallback(ModCallbacks.MC_POST_RENDER, MinimapAPI.AddMapFlag)
 mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.Gowno)
